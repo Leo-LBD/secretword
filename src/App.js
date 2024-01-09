c3795d543b65c839dbfaac72de1714f71d49ad4c
Leo-LBD <leonardo.dinizbassiilio@gmail.com>
2024-01-09 17:19:48 -0300

    Commit 2
    Features: Estabelecidas os estilos, funções de formulários e utilização de States

--- src/App.js
+++ src/App.js
@@ -12,14 +12,11 @@ import StartScreen from './components/StartScreen';
 import Game from './components/Game';
 import GameOver from './components/GameOver';
 
-
 const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
   {id: 3, name: "end"}
 ]
-
-
 
 function App() {
 
@@ -30,21 +27,48 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("")
   const [letters, setLetters] = useState([])
 
-  const pickWordAndCategory = () => {
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState([3])
+  const [score, setScore] = useState([0])
 
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random()* Object.keys(categories).length)]
+
+    console.log(category)
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random()* words[category].length)]
+    console.log(word)
+
+    return {word, category}
   }
 
   //starts the secret word game
   const startGame = () => {
 
     //pick word and pick category 
+    const{word, category} = pickWordAndCategory()
+
+    //Create an array of letters
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+    //fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
 
     setGameStage(stages[1].name)
   }
 
   //process the letter input
-  const verifyLetter = () =>{
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) =>{
+    console.log(letter)
   } 
 
   //Restart the game
@@ -55,7 +79,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'game' && 
+        <Game 
+          verifyLetter={verifyLetter} 
+          pickedWord={pickedWord} 
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+          />}
       {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
